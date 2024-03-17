a394403a6a9bf20d88aa49f39545a7d77cf19448
a7medbehiry <a7medbehiry@gmail.com>
2024-03-17 21:34:49 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AdminDashboard());
+}
+
+class AdminDashboard extends StatelessWidget {
+  const AdminDashboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
