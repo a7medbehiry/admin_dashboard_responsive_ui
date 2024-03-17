6f38307376873d1b624fa71341e606e0f584d6bc
a7medbehiry <a7medbehiry@gmail.com>
2024-03-17 21:48:11 +0200

    preparation

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,26 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'views/dashboard_view.dart';
 
 void main() {
-  runApp(const AdminDashboard());
+  runApp(
+    DevicePreview(
+      enabled: true,
+      builder: (context) => const ResponsiveAdminDashboard(),
+    ),
+  );
 }
 
-class AdminDashboard extends StatelessWidget {
-  const AdminDashboard({super.key});
+class ResponsiveAdminDashboard extends StatelessWidget {
+  const ResponsiveAdminDashboard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      home: const DashboardView(),
+    );
   }
 }
